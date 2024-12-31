c05356e7a342aa469bebd805ff25a324dc2f1fee
heroshg <heros.goodoy@gmail.com>
2024-12-30 23:40:04 -0300

    refact: desfazendo inputmodels desnecessários por conta do CQRS

---
+++ DevFreela.Application/Models/SkillViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.Application.Models
+{
+    public class SkillViewModel
+    {
+        public SkillViewModel(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public static SkillViewModel FromEntity(Skill skill)
+            => new(skill.Id,skill.Description);
+    }
+}
